3e4d91094f50ce08ee09c96e3b9beb55c86d3008
fitbie <fitbiestudio@gmail.com>
2023-09-13 08:06:11 +1100

    refactor: extract ui slots sorter so separate class

---
+++ Scripts/Inventory/UIScripts/Slots/InventoryUISlotsSorter.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Inventory
+{
+
+public class InventoryUISlotsSorter : MonoBehaviour
+{
+
+
+    #region TypeSorting
+
+    // Determines in which order the sorting will proceed. If we add new item typed - update this list.
+    private static List<Type> typeOrder = new List<Type>
+    {
+        typeof(FoodItem),
+        typeof(FuelItem),
+        typeof(ScienceItem),
+    };
+
+
+    public static void SortChildrenByItemType(Dictionary<InventorySlot, InventoryUISlot> currentSlots)
+    {
+        // Get all keys(InventorySlot) and order them using typeOrder.
+        var sortedKeys = currentSlots.Keys.OrderBy(key => typeOrder.IndexOf(key.Item.GetType()));
+
+        // Move slots position accordong to sordedKeys
+        foreach (var key in sortedKeys)
+        {
+            var uiSlot = currentSlots[key];
+            uiSlot.transform.SetAsLastSibling();
+        }
+    }
+
+    #endregion
+
+}
+
+}
