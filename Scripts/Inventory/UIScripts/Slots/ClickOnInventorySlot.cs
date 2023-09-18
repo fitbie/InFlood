74eb1955e40fc1e6fee2235196b070ae0edc0765
fitbie <fitbiestudio@gmail.com>
2023-09-19 03:44:18 +1100

    feat: add click on ui slot handler

---
+++ Scripts/Inventory/UIScripts/Slots/ClickOnInventorySlot.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Inventory
+{
+
+using DisplayMode = InventoryUISlot.DisplayMode;
+
+[RequireComponent(typeof(InventoryUISlot))]
+public class ClickOnInventorySlot : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private InventoryUISlotPanel slotPanelPrefab;
+    private InventoryUISlot uiSlot;
+
+    void Start()
+    {
+        uiSlot = GetComponent<InventoryUISlot>();
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ClickResponce(uiSlot.CurrentDisplayMode);
+    }
+
+
+    public void ClickResponce(DisplayMode displayMode)
+    {
+        switch (displayMode)
+        {
+            case DisplayMode.Default:
+            var slotPanel = Instantiate(slotPanelPrefab, transform.parent.parent);
+            slotPanel.BuildPanel(uiSlot.SlotData.Item);
+            break;
+
+            case DisplayMode.Price:
+            Debug.Log("Sell");
+            break;
+        }
+    }
+}
+
+}
