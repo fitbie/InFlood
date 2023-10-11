d3d8c730d866494542cd60e8f5ee14ef25b4311f
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-10-11 23:15:38 +0700

    feat: add buy tab logic

--- Scripts/Inventory/UIScripts/Tabs/BuyTab.cs
+++ Scripts/Inventory/UIScripts/Tabs/BuyTab.cs
@@ -1,27 +1,46 @@
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace InventorySystem
 {
 
-using DisplayMode = InventoryUISlot.DisplayMode;
+using SlotState = InventoryUISlot.SlotState;
 
-public class BuyTab : MonoBehaviour
+public class BuyTab : InventoryTab
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public static void ShowBuySlots(InventoryPanelBuilder inventoryPanel, List<InventorySlot> merchantSlots)
+    protected override void Start()
     {
-        var currentSlots = inventoryPanel.CurrentSlots;
-        foreach (var element in currentSlots)
-        {
-            var uiSlot = element.Value;
-            uiSlot.gameObject.SetActive(false);
+        base.Start();
 
-            
+        if (Merchant.Current != null) { TabState(Merchant.Current); } // TODO: Initialization ?
+
+        Merchant.OnMerchantTriggered += TabState;
+    }
+
+
+    private void TabState(Merchant merchant)
+    {
+        button.interactable = merchant.IsTriggered;
+    }
+
+
+    public override void OpenTab()
+    {
+        var playerPanel = UserUiManager.Instance.playerInventory;
+        var merchantPanel = UserUiManager.Instance.merchantPanel;
+
+        foreach (var uiSlot in merchantPanel.CurrentSlots.Values)
+        {
+            uiSlot.DiplaySlot(SlotState.Price);
+            uiSlot.gameObject.SetActive(true);
+        }
+
+        foreach (var uiSlot in playerPanel.CurrentSlots.Values)
+        {
+            var go = uiSlot.gameObject;
+            if (go.activeSelf) { go.SetActive(false); }
         }
     }
+
+
+    public override void CloseTab() {}
 
 }
 
