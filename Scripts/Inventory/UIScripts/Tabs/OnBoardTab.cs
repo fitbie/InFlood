546960021c16b7cdc22098832e28c28323bb3cd4
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-10-11 23:16:54 +0700

    feat: add deriving and merchant slots disabling

--- Scripts/Inventory/UIScripts/Tabs/OnBoardTab.cs
+++ Scripts/Inventory/UIScripts/Tabs/OnBoardTab.cs
@@ -1,23 +1,39 @@
 namespace InventorySystem
 {
 
-using DisplayMode = InventoryUISlot.DisplayMode;
+using SlotState = InventoryUISlot.SlotState;
 
-public class OnBoardTab
+public class OnBoardTab : InventoryTab
 {
     /// <summary>
     /// Activates all inventory slots.
     /// </summary>
-    public static void ShowOnBoardSlots(InventoryPanelBuilder inventoryPanel)
+    public override void OpenTab()
     {
-        var currentSlots = inventoryPanel.CurrentSlots;
-        foreach (var element in currentSlots)
+        var playerPanel = UserUiManager.Instance.playerInventory;
+        var merchantPanel = UserUiManager.Instance.merchantPanel;
+
+        foreach (var element in playerPanel.CurrentSlots)
         {
             var uiSlot = element.Value;
             if (!uiSlot.gameObject.activeSelf)  { uiSlot.gameObject.SetActive(true); }
-            uiSlot.DiplaySlot(DisplayMode.Default);
+            uiSlot.DiplaySlot(SlotState.Default);
+        }
+
+        // TODO: for all Tabs make merchant slots non visible more smart way without iteration
+        foreach (var uiSlot in merchantPanel.CurrentSlots.Values)
+        {
+            var go = uiSlot.gameObject; 
+            if (go.activeSelf) { go.SetActive(false); }
         }
     }
+
+
+    public override void CloseTab( )
+    {
+        
+    }
+
 }
 
 }
