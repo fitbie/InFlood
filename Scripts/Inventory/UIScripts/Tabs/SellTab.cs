25daf46f995810102bc38474880037c4c14e9f3f
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-10-11 23:16:38 +0700

    feat: add sell logic and merchant slots handling

--- Scripts/Inventory/UIScripts/Tabs/SellTab.cs
+++ Scripts/Inventory/UIScripts/Tabs/SellTab.cs
@@ -1,22 +1,39 @@
 namespace InventorySystem
 {
-    
-using DisplayMode = InventoryUISlot.DisplayMode;
+    using SlotState = InventoryUISlot.SlotState;
 
-public class SellTab
+public class SellTab : InventoryTab
 {
+    protected override void Start()
+    {
+        base.Start();
+
+        if (Merchant.Current != null) { TabState(Merchant.Current); } // TODO: Initialization ?
+
+        Merchant.OnMerchantTriggered += TabState;
+    }
+
+
+    private void TabState(Merchant merchant)
+    {
+        button.interactable = merchant.IsTriggered;
+    }  
+
+
     /// <summary>
     /// Activates inventory slots that implement IMarketable (we can sell / buy them).
     /// </summary>
-    public static void ShowSellSlots(InventoryPanelBuilder inventoryPanel)
+    public override void OpenTab()
     {
-        var currentSlots = inventoryPanel.CurrentSlots;
-        foreach (var element in currentSlots)
+        var playerPanel = UserUiManager.Instance.playerInventory;
+        var merchantPanel = UserUiManager.Instance.merchantPanel;
+
+        foreach (var element in playerPanel.CurrentSlots)
         {
             if (element.Key.Item is IMarketable) 
             {
                 var uiSlot = element.Value;
-                uiSlot.DiplaySlot(DisplayMode.Price);
+                uiSlot.DiplaySlot(SlotState.Price);
                 uiSlot.gameObject.SetActive(true);
             }
 
@@ -26,7 +43,17 @@ public class SellTab
                 uiSlot.gameObject.SetActive(false);
             }
         }
+
+        // TODO: for all Tabs make merchant slots non visible more smart way without iteration
+        foreach (var uiSlot in merchantPanel.CurrentSlots.Values)
+        {
+            var go = uiSlot.gameObject; 
+            if (go.activeSelf) { go.SetActive(false); }
+        }
     }
+
+
+    public override void CloseTab() {}
 
 }
 
