135b84a3382c239772da13c9e4d41d6224a3658a
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-11-09 22:54:13 +0700

    feat: add Player class

---
+++ Scripts/Characters/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for game characters.
+/// </summary>
+public abstract class Character : MonoBehaviour // TODO
+{
+
+}
