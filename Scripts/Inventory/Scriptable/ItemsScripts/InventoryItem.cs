2494382d3ae347d546b34b97a4625c1255272927
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-11-09 23:10:06 +0700

    doc: add comment

--- Scripts/Inventory/Scriptable/ItemsScripts/InventoryItem.cs
+++ Scripts/Inventory/Scriptable/ItemsScripts/InventoryItem.cs
@@ -15,6 +15,8 @@ public class InventoryItem : ScriptableObject
     [field:SerializeField] public int ID { get; private set; }
     [field: SerializeField] public string Name { get; private set; }
     [field: SerializeField] public string Description { get; private set; }
+    [field: SerializeField] public GameObject ContainerPrefab { get; private set; } // Prefab for spawned loot box
+    [field: SerializeField] public int Rarety { get; private set; } // Loot raraty
     [field: SerializeField] public Sprite SmallIcon { get; private set; } // Icon for inventory slot.
     [field: SerializeField] public Sprite BigIcon { get; private set; } // Icon for item panel.
     [field: SerializeField] public Color TypeColor { get; private set; } // Color for UI slot background + some FX.
