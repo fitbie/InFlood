c7899925e003fb8e4a0e8b2926e1c1d9e8084621
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-02-19 22:43:39 +0700

    feat: interface provides ModulesManager for Entity

---
+++ Scripts/Game Entities/Interfaces/Modules/IHaveModules.cs
@@ -0,0 +1,11 @@
+namespace InFlood.Entities.Modules
+{
+    /// <summary>
+    /// Provides ModulesManager using to implementing entity.
+    /// </summary>
+    public interface IHaveModules
+    {
+        public ModulesManager ModulesManager { get; set; }
+    }
+
+}
