7859140c94fb0d532f987ac1b1fabf7d8e6cc2fd
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-02-19 23:05:59 +0700

    feat: Status Effects Handler, handling all applies effects

---
+++ Scripts/Game Entities/Effects/StatusEffectsHandler.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InFlood.Entities.ActionSystem
+{
+    /// <summary>
+    /// Handles status (temporary) effects. Contain StatusEffectData HashSet, invokes StatusEffectData.Tick() every
+    /// StatusEffectData.Delay seconds, revert expired effects and send OnEffectDataChanged events.
+    /// </summary>
+    public class StatusEffectsHandler : MonoBehaviour
+    {
+        /// <summary>
+        /// Currently handling effects.
+        /// </summary>
+        public HashSet<StatusEffectData> CurrentEffects { get; private set; } = new();
+
+        /// <summary>
+        /// Raised when handle start handling new StatusEffectData.
+        /// </summary>
+        public event Action<StatusEffectData> OnEffectApplied = (e) => Debug.Log($"Handler Applied {e}");
+
+        /// <summary>
+        /// Raised when any StatusEffectData changed, e.g. CurrentTime timer or Concatenate new effect to existing.
+        /// </summary>
+        public event Action<StatusEffectData> OnEffectChanged = (e) => Debug.Log($"Handler Changed {e}");
+
+        /// <summary>
+        /// Raised when effect's lifetime is over, or canceled.
+        /// </summary>
+        public event Action<StatusEffectData> OnEffectReverted = (e) => Debug.Log($"Handler Revert {e}");
+
+        /// <summary>
+        /// Pass here StatusEffectData instance to starts its handling process.
+        /// If there is no Equal() effect in CurrentEffects - add new and start handle timer. If there is -
+        /// Concatenates containing and new effect into containing, e.g. prolongs its timer and other logic.
+        /// </summary>
+        /// <param name="effect"></param>
+        public void HandleEffect(StatusEffectData effect)
+        {
+            if (!CurrentEffects.TryGetValue(effect, out var contained)) 
+            { 
+                CurrentEffects.Add(effect);
+                OnEffectApplied?.Invoke(effect);
+                StartCoroutine(EffectHandleRoutine(effect));
+            }
+            else 
+            { 
+                contained.Concat(effect);
+                OnEffectChanged?.Invoke(contained); 
+            }
+        }
+
+        /// <summary>
+        /// Counts effect's data timer and invokes Tick(). When CurrentTime is over - removes effect from HashSet and
+        /// send OnEffectReverted.
+        /// </summary>
+        private IEnumerator EffectHandleRoutine(StatusEffectData effect)
+        {
+            yield return null; // Skip first Tick() when effect just been applied.
+
+            while (effect.CurrentTime > 0)
+            {
+                effect.Tick();
+                OnEffectChanged?.Invoke(effect);
+                effect.CurrentTime -= effect.Delay;
+                yield return new WaitForSeconds(effect.Delay);
+            }
+
+            effect.RevertEffect();
+            OnEffectReverted?.Invoke(effect);
+
+            CurrentEffects.Remove(effect);
+        }
+    }
+
+}
