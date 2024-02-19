da0ffb62c681cf2301430c3b2f0ae1c75e401016
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-02-19 22:40:28 +0700

    fix: for init keyword in properties

---
+++ Scripts/Utils/IsExternalInit.cs
@@ -0,0 +1,7 @@
+namespace System.Runtime.CompilerServices
+    {
+        /// <summary>
+        /// C# Bugfix for using init keyword in properties.
+        /// </summary>
+        internal static class IsExternalInit {}
+    }
