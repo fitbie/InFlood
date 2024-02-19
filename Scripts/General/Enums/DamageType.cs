440a49a93518a867087a68658aa98c92c88aa2c9
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-02-19 22:46:18 +0700

    feat: enum describing all damage types in InFlood

---
+++ Scripts/General/Enums/DamageType.cs
@@ -0,0 +1,22 @@
+namespace InFlood.Entities.ActionSystem
+{
+    /// <summary>
+    /// All types of Damage presented in InFlood.
+    /// </summary>
+    public enum DamageType
+    {
+        /// <summary>
+        /// Damage of bullets and projectiles.
+        /// </summary>
+        Kinetic,
+        /// <summary>
+        /// All fire-related things, e.g. flamethrowers and incendiary projectiles.
+        /// </summary>
+        Thermal,
+        /// <summary>
+        /// All kinds of other energy, e.g. blasters, lasers, electomagnetic pulses.
+        /// </summary>
+        Energetic
+    }
+
+}
