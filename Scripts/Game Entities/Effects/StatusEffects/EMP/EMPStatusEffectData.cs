d556c734b8e8db347f3843f8fab76e347ea98cba
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-02-28 22:09:30 +0700

    fix: EMP status effect data applied once in Tick() instead constuctor

--- Scripts/Game Entities/Effects/StatusEffects/EMP/EMPStatusEffectData.cs
+++ Scripts/Game Entities/Effects/StatusEffects/EMP/EMPStatusEffectData.cs
@@ -3,31 +3,27 @@ namespace InFlood.Entities.ActionSystem
     public class EMPStatusEffectData : StatusEffectData
     {
         private readonly IEMPSensitive target;
+        private bool isApplied; 
 
 
         public EMPStatusEffectData(Effect original, float duration, float delay, IEMPSensitive target) : 
-        base(original, duration, delay)
-        {
-            this.target = target;
-            target.ApplyEMP(true);
-        }
+        base(original, duration, delay) => this.target = target;
+
 
         public override void Tick()
         {
-            // No need to tick for EMP.
+            if (!isApplied)
+            {
+                target.ApplyEMP(true);
+                isApplied = true;
+            }
         }
 
 
-        public override void RevertEffect()
-        {
-            target.ApplyEMP(false);
-        }
+        public override void RevertEffect() => target.ApplyEMP(false);
 
 
-        public override string ToString()
-        {
-            return $"EMP Effect: Current time: {CurrentTime}, duration: {Duration}, delay: {Delay} ";
-        }
+        public override string ToString() => $"EMP Effect: Current time: {CurrentTime}, duration: {Duration}, delay: {Delay} ";
 
     }
 
